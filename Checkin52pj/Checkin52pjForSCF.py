1da3c7d47f51a30f282c37de7aa350bd805d942f
mengdie101 <66658144+mengdie101@users.noreply.github.com>
2020-11-13 09:32:35 -0600

    Update Checkin52pjForSCF.py

--- Checkin52pj/Checkin52pjForSCF.py
+++ Checkin52pj/Checkin52pjForSCF.py
@@ -6,7 +6,7 @@ cookie = os.environ.get('cookie_52pj')
 
 def pjCheckin(*args):
     try:
-        SCKEY = os.environ.get('PUSH_KEY')
+        SCKEY = os.environ.get('SCKEY')
         s = requests.Session()
         headers={
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
